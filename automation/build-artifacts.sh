2554a54f9a3c390879c502438e9bc578a2cd8725
Fabian Deutsch <fabiand@fedoraproject.org>
2015-12-08 12:10:07 +0100

    automation: Simplify build
    Change-Id: I5c5a29e0320d484bcac727cc1e665463ca312ba9
    Signed-off-by: Fabian Deutsch <fabiand@fedoraproject.org>

--- automation/build-artifacts.sh
+++ automation/build-artifacts.sh
@@ -27,28 +27,28 @@ build() {
 }
 
 prepare_boot() {
+  #
+  # Some preparations to allow installing the image remotely from jenkins
+  #
+
   # The squashfs url is now pointing to the image in the jenkins instance
   local SQUASHFS_URL="$JOB_URL/lastSuccessfulBuild/artifact/exported-artifacts/ovirt-node-ng.squashfs.img"
 
   # Also update the kickstarts to point to that url
-  sed "s#@SQUASHFS_URL@#$SQUASHFS_URL#" interactive-installation.ks.in > interactive-installation.ks
-  sed "s#@SQUASHFS_URL@#$SQUASHFS_URL#" ovirt-node-ng-auto-installation.ks.in > ovirt-node-ng-auto-installation.ks
-  sed -i -e "/http_proxy=/ d" -e "s/^poweroff/reboot/" *-installation.ks
+  sed "s#@SQUASHFS_URL@#$SQUASHFS_URL#" docs/ks-examples/interactive-installation.ks.in > "$ARTIFACTSDIR"/interactive-installation.ks
+  sed "s#@SQUASHFS_URL@#$SQUASHFS_URL#" data/ovirt-node-ng-auto-installation.ks.in > "$ARTIFACTSDIR"/ovirt-node-ng-auto-installation.ks
+  sed -i -e "/http_proxy=/ d" -e "s/^poweroff/reboot/" "$ARTIFACTSDIR"/*-installation.ks
 
-  bash image-tools/bootstrap_anaconda centos 7
+  pushd "$ARTIFACTSDIR"/
+    # Anaconda uses the .treeinfo file to find stuff
+    curl -O http://mirror.centos.org/centos-7/7/os/x86_64/.treeinfo
+    # Let the squashfs point to the PWD, not in some subdir
+    sed -i -e "s#=.*images/pxeboot/#= #" \
+           -e "s#=.*LiveOS/#= #" \
+           .treeinfo
+  popd
 
-  mv -v \
-    *.ks .treeinfo \
-    "$ARTIFACTSDIR/"
-
-  # FIXME these files should to go to images/ at some point as well
-  mv -v \
-    vmlinuz initrd.img squashfs.img upgrade.img \
-    "$ARTIFACTSDIR/"
-
-  mkdir -p "$ARTIFACTSDIR/images/"
-
-  ls -shal "$ARTIFACTSDIR/" "$ARTIFACTSDIR/images" || :
+  ls -shal "$ARTIFACTSDIR/" || :
 }
 
 prepare
