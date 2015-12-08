d09af9a5158d947a31e1bef66380d8e12a9a2940
Fabian Deutsch <fabiand@fedoraproject.org>
2015-12-08 17:56:39 +0100

    ci: Drop prepare_build for now
    Change-Id: I6d9cd6a4006f4df4674a6762f2f12c97c6a1d330
    Signed-off-by: Fabian Deutsch <fabiand@fedoraproject.org>

--- automation/build-artifacts.sh
+++ automation/build-artifacts.sh
@@ -26,31 +26,5 @@ build() {
   ls -shal "$ARTIFACTSDIR/" || :
 }
 
-prepare_boot() {
-  #
-  # Some preparations to allow installing the image remotely from jenkins
-  #
-
-  # The squashfs url is now pointing to the image in the jenkins instance
-  local SQUASHFS_URL="$JOB_URL/lastSuccessfulBuild/artifact/exported-artifacts/ovirt-node-ng.squashfs.img"
-
-  # Also update the kickstarts to point to that url
-  sed "s#@SQUASHFS_URL@#$SQUASHFS_URL#" docs/ks-examples/interactive-installation.ks.in > "$ARTIFACTSDIR"/interactive-installation.ks
-  sed "s#@SQUASHFS_URL@#$SQUASHFS_URL#" data/ovirt-node-ng-auto-installation.ks.in > "$ARTIFACTSDIR"/ovirt-node-ng-auto-installation.ks
-  sed -i -e "/http_proxy=/ d" -e "s/^poweroff/reboot/" "$ARTIFACTSDIR"/*-installation.ks
-
-  pushd "$ARTIFACTSDIR"/
-    # Anaconda uses the .treeinfo file to find stuff
-    curl -O http://mirror.centos.org/centos-7/7/os/x86_64/.treeinfo
-    # Let the squashfs point to the PWD, not in some subdir
-    sed -i -e "s#=.*images/pxeboot/#= #" \
-           -e "s#=.*LiveOS/#= #" \
-           .treeinfo
-  popd
-
-  ls -shal "$ARTIFACTSDIR/" || :
-}
-
 prepare
 build
-prepare_boot
