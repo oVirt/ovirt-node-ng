7ce9d37574458174e25c5c5a5d59624e6f8bcf92
Fabian Deutsch <fabiand@fedoraproject.org>
2016-02-23 18:26:51 +0100

    scripts: Add a scripts to create a bootable installation isos
    This script takes a existing CentOS 7 iso and add's the given
    squashfs and a minimalisitc script to generate a new and working
    CentOS 7 iso for Node Next.
    
    Usage:
    $ bash derive-boot-iso.sh boot.iso ovirt-node-ng-image.squashfs.img
    
    boot.iso and ovirt-node-ng-image.squashfs.img need to exist.
    
    A convenience script is create-nightly-boot-iso.sh which will pull
    a CentOS 7 installation ISO and the correct squashfs and generate
    the bootable Node Next iso on the fly.
    
    Usage:
    $ bash create-nightly-boot-iso.sh
    
    Change-Id: Ic3ed464a415118887777edf9b55427cd5ab4ca2d
    Signed-off-by: Fabian Deutsch <fabiand@fedoraproject.org>

---
+++ scripts/derive-boot-iso.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+# Usage: bash derive-boot-iso.sh boot.iso ovirt-node-ng-image.squashfs.img
+
+set -e
+
+BOOTISO=$(realpath $1)
+SQUASHFS=$(realpath $2)
+NEWBOOTISO=${3:-$(dirname $BOOTISO)/new-$(basename $BOOTISO)}
+
+TMPDIR=$(realpath bootiso.d)
+
+extract_iso() {
+  echo "[1/4] Extracting ISO"
+  local ISOFILES=$(isoinfo -i $BOOTISO -RJ -f | sort -r | egrep "/.*/")
+  for F in $ISOFILES
+  do
+    mkdir -p ./$(dirname $F)
+    [[ -d .$F ]] || ( isoinfo -i $BOOTISO -RJ -x $F > .$F )
+  done
+}
+
+add_payload() {
+  echo "[2/4] Adding squashfs to ISO"
+  local DST=$(basename $SQUASHFS)
+  cp $SQUASHFS $DST
+  echo "liveimg --url=file:///run/install/repo/$DST" > liveimg.ks
+}
+
+modify_bootloader() {
+  echo "[3/4] Updating bootloader"
+  # grep -rn stage2 *
+  local CFGS="EFI/BOOT/grub.cfg isolinux/isolinux.cfg"
+  local PRODURL="http://jenkins.ovirt.org/job/fabiand_boo_build_testing/lastSuccessfulBuild/artifact/product.img"
+  sed -i "/stage2/ s%$% inst.ks=cdrom:/liveimg.ks%" $CFGS
+  sed -i "/stage2/ s%$% inst.updates=$PRODURL%" $CFGS
+}
+
+create_iso() {
+  echo "[4/4] Creating new ISO"
+  local volid=$(isoinfo -d -i $BOOTISO | grep "Volume id" | cut -d ":" -f2 | sed "s/^ //")
+  mkisofs -J -T -o $NEWBOOTISO -b isolinux/isolinux.bin -c isolinux/boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table -R -graft-points -V "$volid" $TMPDIR > mkisofs.log 2>&1 || { cat mkisofs.log ; exit 1 ; } && rm mkisofs.log
+}
+
+main() {
+  mkdir $TMPDIR
+  cd $TMPDIR
+
+  extract_iso
+  add_payload
+  modify_bootloader
+  create_iso
+
+  rm -rf $TMPDIR
+}
+
+main
