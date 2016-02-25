008bb10b46005534eb8d8b4540fa650e69249a73
Fabian Deutsch <fabiand@fedoraproject.org>
2016-02-25 07:57:56 +0100

    scripts: Fetch helper script if necessary
    Change-Id: Ied29d279f8c514aaaad5f0f568101d1155c8769e
    Signed-off-by: Fabian Deutsch <fabiand@fedoraproject.org>

--- scripts/create-nightly-boot-iso.sh
+++ scripts/create-nightly-boot-iso.sh
@@ -9,6 +9,7 @@ CLEAN=
 cond_curl() {
   if [[ -e "$1" ]]; then echo "Reusing existing $1" ;
   else echo "Fetching $1" ; curl -# -o "$1" $2 ; CLEAN="$CLEAN $1" ; fi
+  [[ -n "$3" ]] && $3 $1
 }
 
 echo "Building an oVirt Node Next boot.iso"
@@ -16,6 +17,7 @@ echo "from CentOS 7 boot.iso and a nightly squashfs"
 echo "This can take a while ..."
 cond_curl $BOOTISO http://mirror.centos.org/centos/7/os/x86_64/images/boot.iso
 cond_curl $SQUASHFS http://jenkins.ovirt.org/job/ovirt-node-ng_master_build-artifacts-fc22-x86_64/lastStableBuild/artifact/exported-artifacts/ovirt-node-ng-image.squashfs.img
+cond_curl derive-boot-iso.sh "https://gerrit.ovirt.org/gitweb?p=ovirt-node-ng.git;a=blob_plain;f=scripts/derive-boot-iso.sh" "chmod a+x"
 
 ./derive-boot-iso.sh "$BOOTISO" "$SQUASHFS" "$NEWISO"
 
