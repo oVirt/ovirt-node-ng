271aa811dda8d8fa8881859c669e9f86994dff5d
Fabian Deutsch <fabiand@fedoraproject.org>
2016-04-07 12:23:47 +0200

    ci: Make branch configurable
    Change-Id: I9a87b728e6ed24d3801ca79043849df2c8d83550
    Signed-off-by: Fabian Deutsch <fabiand@fedoraproject.org>

--- automation/build-artifacts.sh
+++ automation/build-artifacts.sh
@@ -9,6 +9,8 @@ export CURLOPTS="-x http://proxy.phx.ovirt.org:3128"
 export TMPDIR=$PWD/tmp
 export LIBGUESTFS_BACKEND=direct
 
+export BRANCH=master
+
 prepare() {
   virt-host-validate || :
 
@@ -27,7 +29,7 @@ build() {
 cat > /etc/yum.repos.d/ovirt-node.repo <<__EOR__
 [ovirt-node-nightly]
 name=oVirt Node Next (Nightly)
-baseurl=http://jenkins.ovirt.org/job/ovirt-node-ng_master_build-artifacts-fc22-x86_64/lastSuccessfulBuild/artifact/exported-artifacts/
+baseurl=http://jenkins.ovirt.org/job/ovirt-node-ng_${BRANCH}_build-artifacts-fc22-x86_64/lastSuccessfulBuild/artifact/exported-artifacts/
 enabled=1
 gpgcheck=0
 __EOR__
