58b288eafd34f60a8596d9e62aefe589e178b551
Fabian Deutsch <fabiand@fedoraproject.org>
2016-04-09 15:21:29 +0200

    scripts: Fix branding inclusion
    Change-Id: I1034f4445893e895b623b68ff296991ff5218396
    Signed-off-by: Fabian Deutsch <fabiand@fedoraproject.org>

--- scripts/create-nightly-boot-iso.sh
+++ scripts/create-nightly-boot-iso.sh
@@ -5,6 +5,7 @@ NEWISO=${1:-$(realpath .)/ovirt-node-ng-installer-${BRANCH}-$(date +%Y%m%d%H).is
 BOOTISO=${BOOTISO:-boot.iso}
 SQUASHFS=${SQUASHFS:-ovirt-node-ng-image.squashfs.img}
 DERVICEBOOTISOSCRIPT=${DERVICEBOOTISOSCRIPT:-derive-boot-iso.sh}
+PRODUCTIMG=product.img
 CLEAN=
 
 cond_curl() {
@@ -16,13 +17,14 @@ cond_curl() {
 echo "Building an oVirt Node Next boot.iso"
 echo "from CentOS 7 boot.iso and a nightly squashfs"
 echo "This can take a while ..."
-cond_curl $BOOTISO http://mirror.centos.org/centos/7/os/x86_64/images/boot.iso
-cond_curl $SQUASHFS http://jenkins.ovirt.org/job/ovirt-node-ng_${BRANCH}_build-artifacts-fc22-x86_64/lastStableBuild/artifact/exported-artifacts/ovirt-node-ng-image.squashfs.img
-cond_curl $DERVICEBOOTISOSCRIPT "https://gerrit.ovirt.org/gitweb?p=ovirt-node-ng.git;a=blob_plain;f=scripts/derive-boot-iso.sh" "chmod a+x"
+cond_curl "$BOOTISO" "http://mirror.centos.org/centos/7/os/x86_64/images/boot.iso"
+cond_curl "$SQUASHFS" "http://jenkins.ovirt.org/job/ovirt-node-ng_${BRANCH}_build-artifacts-fc22-x86_64/lastStableBuild/artifact/exported-artifacts/ovirt-node-ng-image.squashfs.img"
+cond_curl "$PRODUCTIMG" "http://jenkins.ovirt.org/job/fabiand_boo_build_testing/lastSuccessfulBuild/artifact/${BRANCH}-product.img"
+cond_curl "$DERVICEBOOTISOSCRIPT" "https://gerrit.ovirt.org/gitweb?p=ovirt-node-ng.git;a=blob_plain;f=scripts/derive-boot-iso.sh" "chmod a+x"
 
 {
   set -e
-  bash $DERVICEBOOTISOSCRIPT "$BOOTISO" "$SQUASHFS" "$NEWISO"
+  PRODUCTIMG=$PWD/$PRODUCTIMG bash $DERVICEBOOTISOSCRIPT "$BOOTISO" "$SQUASHFS" "$NEWISO"
   echo "New installation ISO: $NEWISO"
   [[ -n "$CLEAN" ]] && rm $CLEAN || :
 }
