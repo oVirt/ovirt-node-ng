cac35d44c032d46d64854ba0bf2e4dbad748e209
Fabian Deutsch <fabiand@fedoraproject.org>
2016-05-06 16:33:00 +0200

    scripts: More porduct.img fixes
    Change-Id: I94175f8afc28bbb0d42b99eb8076d53315325e2b
    Signed-off-by: Fabian Deutsch <fabiand@fedoraproject.org>

--- scripts/create-nightly-boot-iso.sh
+++ scripts/create-nightly-boot-iso.sh
@@ -4,6 +4,7 @@ BRANCH=${BRANCH:-master}
 NEWISO=${1:-$(realpath .)/ovirt-node-ng-installer-${BRANCH}-$(date +%Y%m%d%H).iso}
 BOOTISO=${BOOTISO:-boot.iso}
 SQUASHFS=${SQUASHFS:-ovirt-node-ng-image.squashfs.img}
+PRODUCTIMG=${PRODUCTIMG:-product.img}
 DERVICEBOOTISOSCRIPT=${DERVICEBOOTISOSCRIPT:-derive-boot-iso.sh}
 CLEAN=
 
@@ -18,12 +19,12 @@ echo "from CentOS 7 boot.iso and a nightly squashfs"
 echo "This can take a while ..."
 cond_curl "$BOOTISO" "http://mirror.centos.org/centos/7/os/x86_64/images/boot.iso"
 cond_curl "$SQUASHFS" "http://jenkins.ovirt.org/job/ovirt-node-ng_${BRANCH}_build-artifacts-fc22-x86_64/lastStableBuild/artifact/exported-artifacts/ovirt-node-ng-image.squashfs.img"
-# FIXME Also fetch product.img from the squashfs job
+cond_curl "$PRODUCTIMG" "http://jenkins.ovirt.org/job/ovirt-node-ng_${BRANCH}_build-artifacts-fc22-x86_64/lastStableBuild/artifact/exported-artifacts/product.img"
 cond_curl "$DERVICEBOOTISOSCRIPT" "https://gerrit.ovirt.org/gitweb?p=ovirt-node-ng.git;a=blob_plain;f=scripts/derive-boot-iso.sh" "chmod a+x"
 
 {
   set -e
-  bash $DERVICEBOOTISOSCRIPT "$BOOTISO" "$SQUASHFS" "$NEWISO"
+  PRODUCTIMG=$PRODUCTIMG bash $DERVICEBOOTISOSCRIPT "$BOOTISO" "$SQUASHFS" "$NEWISO"
   echo "New installation ISO: $NEWISO"
   [[ -n "$CLEAN" ]] && rm $CLEAN || :
 }
