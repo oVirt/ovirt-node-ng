0f433caf8210eb8d7488c0a6e07465c00b1f0f6b
Sandro Bonazzola <sbonazzo@redhat.com>
2016-12-22 07:28:30 +0100

    packaging: move to 4.1 repositories
    - moved to ovirt-4.1 repository
    - check for ovirt-release41 rpm
    
    Change-Id: Ib40ec65f1e5f3658fafaad7f09bea374db1a2c89
    Signed-off-by: Sandro Bonazzola <sbonazzo@redhat.com>
    (cherry picked from commit 4a4bdeed0c1b3700a8ce17bf34b2f2294a20c6f9)

--- scripts/generate-manifest.py
+++ scripts/generate-manifest.py
@@ -43,7 +43,7 @@ if __name__ == "__main__":
     rpm = []
     key_pkgs = ['cockpit-ovirt-dashboard', 'kernel',
                 'redhat-release-virtualization-host', 'vdsm',
-                'ovirt-hosted-engine-setup', 'ovirt-release41-snapshot']
+                'ovirt-hosted-engine-setup', 'ovirt-release41']
 
     if len(sys.argv) != 3:
         print("Generates the manifest file from different squashfs images\n")
